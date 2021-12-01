3df5d33b13dbfe3a8080fe6f6199663e7a56c315
pandorum2096 <49597462+pandorum2096@users.noreply.github.com>
2021-12-01 12:47:46 +0000

    commit 2.0

--- services/products.js
+++ services/products.js
@@ -2,13 +2,11 @@ const db = require('./db');
 const helper = require('../helper');
 const config = require('../config');
 
-async function getMultiple(page = 1) {
-  //const offset = helper.getOffset(page, config.listPerPage);
+async function getMultiple() {
   const rows = await db.query(
     'SELECT * FROM product'
   );
   const data = helper.emptyOrRows(rows);
-  //const meta = {page};
 
   return {data}
 }
