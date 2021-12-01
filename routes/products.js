445e0e57d70f02623ca244af0a49a98a05012277
pandorum2096 <49597462+pandorum2096@users.noreply.github.com>
2021-12-01 16:09:16 +0000

    commit 9.0.0

--- routes/products.js
+++ routes/products.js
@@ -34,10 +34,10 @@ router.post('/products', async function(req, res, next) {
 });
 
 /* PUT a product */
-router.put('/products/:id', async function(req, res, next) {
-  const {id} = req.params;
+router.put('/products', async function(req, res, next) {
+  //const {id} = req.params;
   try {
-    res.json(await products.update(req.body, id));
+    res.json(await products.update(req.body));
   } catch (err) {
     console.error(`Error while posting product `, err.message);
     res.status(err.statusCode || 500).json({'message': err.message});
--- services/products.js
+++ services/products.js
@@ -38,7 +38,7 @@ async function create(product){
 }
 
 //PUT a product
-async function update(product, id){
+async function update(product){
 
   const result = await db.query("UPDATE Product SET name=$1,description=$2,price=$3,inStock=$4 WHERE id=$5", [product.name, product.description, product.price, product.inStock, product.id]);
   let message = 'Error in creating product';
